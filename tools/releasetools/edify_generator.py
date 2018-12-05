86768a3e38d8db2c6d6669b30f7b828dd7a38072
LuK1337 <priv.luk@gmail.com>
2018-12-05 20:51:49 +0100

    build: Support system-as-root A-only backuptool
    Change-Id: Ia24ae288a48721f724bfebadfe023330c4932633

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -155,9 +155,10 @@ class EdifyGenerator(object):
            '; this device has bootloader " + getprop("ro.bootloader") + ".");' +
            ");")
     self.script.append(self.WordWrap(cmd))
-    
-  def RunBackup(self, command):
-    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
+
+  def RunBackup(self, command, system_path):
+    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s", "%s");' % (
+        command, system_path)))
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
